52a683deb48183705835a19f9eb534d463658540
Ryan Ford <fordryan01@hotmail.co.uk>
2026-04-08 21:29:17 +0100

    Fixing an error with registration after adding the change password function

--- user/forms.py
+++ user/forms.py
@@ -18,8 +18,3 @@ class CustomSignupForm(SignupForm):
         profile.save()
 
         return user
-
-class CustomPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(label='Old Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    new_password1 = forms.CharField(label='New Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    new_password2 = forms.CharField(label='Confirm New Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
